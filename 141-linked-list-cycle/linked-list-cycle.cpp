9ff589dd18d6aaa933804b607794f3785f79d771
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-04-19 15:13:16 +0530

    Time: 7 ms (83.54%) | Memory: 10.5 MB (61.53%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,34 +9,35 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // if(head==NULL || head->next==NULL){
-        //     return false;
-        // }
-        // ListNode* slow = head;
-        // ListNode* fast = head;
-        // while(fast!=NULL && slow!=NULL){
-        //     fast = fast->next;
-        //     if(fast!=NULL){
-        //         fast = fast->next;
-        //     }
-        //     slow = slow->next;
-        // if(fast == slow){
-        //     return true;
-        // }
-        // }
-        // return false;
-        if(head==NULL){
-            return NULL;
+        if(head==NULL || head->next==NULL){
+            return false;
         }
-        ListNode* temp = head;
-        map<ListNode*, bool> visited;
-        while(temp!=NULL){
-            if(visited[temp]==true){
-                return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && slow!=NULL){
+            fast = fast->next;
+            if(fast!=NULL){
+                fast = fast->next;
             }
-            visited[temp]=true;
-            temp = temp->next;
+            slow = slow->next;
+        if(fast == slow){
+            return true;
+        }
         }
         return false;
+    //     if(head==NULL){
+    //         return NULL;
+    //     }
+    //     ListNode* temp = head;
+    //     map<ListNode*, bool> visited;
+    //     while(temp!=NULL){
+    //         if(visited[temp]==true){
+    //             return true;
+    //         }
+    //         visited[temp]=true;
+    //         temp = temp->next;
+    //     }
+    //     return false;
+    // }
     }
 };
