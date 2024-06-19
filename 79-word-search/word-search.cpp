b586aea7b89070c7b472ae67cd9ec51bc69b0921
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-06-19 14:20:17 +0530

    Time: 648 ms (49.87%) | Memory: 10.7 MB (59.51%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+    bool flag;
+
+    void solve(int i, int j, int idx, vector<vector<bool>>&visited,vector<vector<char>>&board, string &word){
+        if(idx==word.length()){
+            flag=true;
+            return;
+        }
+        int n = board.size();
+        int m = board[0].size();
+        if(i<0 or i>=n or j<0 or j>=m or visited[i][j]){
+            return;
+        }
+        if(word[idx]==board[i][j]){
+            visited[i][j]=true;
+            solve(i+1,j,idx+1,visited,board,word);
+            solve(i-1,j,idx+1,visited,board,word);
+            solve(i,j+1,idx+1,visited,board,word);
+            solve(i,j-1,idx+1,visited,board,word);
+            visited[i][j]=false;
+        }
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
-                }
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        flag=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                solve(i,j,0,visited,board,word);
             }
         }
-        return false;
+        return flag;
     }
 };
