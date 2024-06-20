33fe9750771a3da281c72d90d2d9d5e391fc29a0
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-06-20 14:46:20 +0530

    Time: 3 ms (59.57%) | Memory: 13.5 MB (37.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-    int pivotInd(vector<int>& nums){
+    int binsearch(vector<int>& arr,int target) {
+        int n = arr.size();
         int s = 0;
-        int e = nums.size()-1;
-        int mid = s + (e-s)/2;
-        while(s<e){
-            if(nums[mid]>=nums[0]){
-                s = mid+1;
-            }else{
-                e = mid;
-            }
-           mid = s + (e-s)/2; 
-        }
-        return s;
-    }
-    int BinarySearch(vector<int>& nums, int s,int e , int target){
-        int mid = s+ (e-s)/2;
-        while(s<=e){
-            if(nums[mid]==target){
-                return mid;
-            }else if(target<nums[mid]){
+        int e = n-1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                break;
+            } if(arr[s]<=arr[mid]){ 
+            if(arr[s]<=target && arr[mid]>target){
                 e = mid-1;
+                }else{
+                    s = mid+1;
+                }
             }else{
-                s = mid+1;
+                if(arr[e]>=target && arr[mid]<target){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
             }
-            mid = s+ (e-s)/2;
-        }
-        return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int p = pivotInd(nums);
-        int ans;
-        if(target>=nums[p] && target<=nums[n-1]){
-            ans = BinarySearch(nums, p, n-1,target);
-        }else{
-            ans = BinarySearch(nums, 0 , p-1, target);
         }
         return ans;
     }
-};
+    int search(vector<int>& nums, int target) {
+        int ans = binsearch(nums,target);
+        return ans;
+    }
+    };
