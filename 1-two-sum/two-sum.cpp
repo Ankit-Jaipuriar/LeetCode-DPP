04f38902d9258a62961cf047d892d2ecc4e5715e
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-06-30 14:49:01 +0530

    Time: 15 ms (50.16%) | Memory: 14.9 MB (10.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int k) {
-        int n = nums.size();
-        int s = 0;
-        int e = n - 1;
-        while (s <= e) {
-            int m = (s + e) / 2;
-            if (nums[m] == k) {
-                return m;
-            } else if (nums[m] > k) {
-                e = m - 1;
-            } else {
-                s = m + 1;
+    vector<int> ans;
+    void solve(vector<int>& nums, int target){
+        map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            int rem = target - nums[i];
+            if(mp.find(rem)!=mp.end()){
+                ans.push_back(mp[rem]);
+                ans.push_back(i);
             }
+            mp[nums[i]]=i;
         }
-        return -1;
     }
-
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> res;
-        vector<int> temp = nums;
-        sort(temp.begin(), temp.end());
-        
-        for (int i = 0; i < n; i++) {
-            int x = target - nums[i];
-            int idx = search(temp, x);
-            if (idx != -1) {
-                for (int j = 0; j < n; j++) {
-                    if (temp[idx] == nums[j] && j != i) {
-                        res.push_back(i);
-                        res.push_back(j);
-                        return res;
-                    }
-                }
-            }
-        }
-        return res;
+        solve(nums, target);
+        return ans;
     }
 };
