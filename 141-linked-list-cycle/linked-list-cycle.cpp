12c8918369258e34041663268e6690797aa7d9f4
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-06-30 16:01:33 +0530

    Time: 8 ms (59.68%) | Memory: 10.5 MB (64.19%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,35 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL){
-            return false;
-        }
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast!=NULL && slow!=NULL){
-            fast = fast->next;
-            if(fast!=NULL){
-                fast = fast->next;
-            }
+        while(fast && fast->next){
             slow = slow->next;
-        if(fast == slow){
-            return true;
-        }
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
         }
         return false;
-    //     if(head==NULL){
-    //         return NULL;
-    //     }
-    //     ListNode* temp = head;
-    //     map<ListNode*, bool> visited;
-    //     while(temp!=NULL){
-    //         if(visited[temp]==true){
-    //             return true;
-    //         }
-    //         visited[temp]=true;
-    //         temp = temp->next;
-    //     }
-    //     return false;
-    // }
     }
 };
