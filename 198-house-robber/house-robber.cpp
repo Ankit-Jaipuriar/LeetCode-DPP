e9ecccf3fe9c306f9a65b0005116ad6891f15cf4
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-07-11 15:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (10.76%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,7 +3,7 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+2);
-        dp[n]=0,dp[n+1]=0;
+        // dp[n]=0,dp[n+1]=0;
         for(int i=n-1;i>=0;i--){
            int ans=0;
             ans=max(ans,dp[i+1]);  //skip
