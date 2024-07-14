b0450ba03384e608871dd1f18024e18838bcc2df
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-07-14 16:07:57 +0530

    Time: 4 ms (73.20%) | Memory: 11.7 MB (6.52%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    int helper(int i, int j,int n,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-        if(i==n){
-            return 0;
-        }
-        if(dp[i][j]!=INT_MIN){
-            return dp[i][j];
-        }
-        int down = triangle[i][j] + helper(i+1,j,n,triangle,dp);
-        int diag = triangle[i][j] + helper(i+1,j+1,n,triangle,dp);
-        return dp[i][j]=min(down, diag);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[0].size();
