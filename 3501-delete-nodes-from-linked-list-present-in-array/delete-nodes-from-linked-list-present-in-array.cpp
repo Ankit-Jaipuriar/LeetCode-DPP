eba6ebcb0e1491a1555eea6fd292b704357067b9
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-07-14 11:15:32 +0530

    Time: 454 ms (25.00%) | Memory: 262.8 MB (75.00%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -10,32 +10,15 @@
  */
 class Solution {
 public:
-    // ListNode* helper(ListNode* head, int k){
-    //     ListNode* dummy = new ListNode(-1);
-    //     dummy->next = head;
-    //     ListNode* prev = dummy;
-    //     ListNode* temp = head;
-    //     while(temp!=NULL){
-    //         if(temp->val==k){
-    //             prev->next = temp->next;
-    //             delete temp;
-    //             temp = prev->next;
-    //         }else{
-    //             prev = temp;
-    //             temp = temp->next;
-    //         }
-    //     }
-    //     return dummy->next;
-    // }
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-         unordered_set<int> toRemove(nums.begin(), nums.end());
+        unordered_set<int> st(nums.begin(), nums.end());
         ListNode* dummy = new ListNode(-1);
         dummy->next = head;
         ListNode* prev = dummy;
         ListNode* temp = head;
         
         while (temp != nullptr) {
-            if (toRemove.find(temp->val) != toRemove.end()) {
+            if (st.find(temp->val) != st.end()) {
                 prev->next = temp->next;
                 delete temp;
                 temp = prev->next;
