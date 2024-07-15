4a4ff1f559ac89e172702048b52dcd59d4b3fcff
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-07-15 14:45:16 +0530

    Time: 11 ms (54.48%) | Memory: 20.2 MB (22.71%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     // int count=0;
     int solve(int i,int curr_sum,int k,vector<int>& coins,vector<vector<int>>& dp){
-        if(curr_sum==k){
+        if(i==coins.size()){
+            if(curr_sum==k){
             return 1;
-        } 
-         if (curr_sum > k || i >= coins.size()) {
-            return 0;
+            }else{
+                return 0;
+            }
         }
            if(dp[i][curr_sum]!=-1){
             return dp[i][curr_sum];
@@ -16,7 +17,21 @@ public:
         return dp[i][curr_sum]=(a+b);
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        return solve(0,0,amount,coins,dp);
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
+        int n = coins.size(); 
+        // Initialize base case
+        dp[n][amount] = 1;
+        for (int i = 0; i < amount; i++) {
+            dp[n][i] = 0;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = amount; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j];
+                if (j + coins[i] <= amount) {
+                    dp[i][j] += dp[i][j + coins[i]];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
