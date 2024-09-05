dd8f8d333534045bee2390112eaa1009124214e4
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-09-05 08:18:42 +0530

    Time: 4 ms (38.70%) | Memory: 9.4 MB (81.73%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void dfs(int num, int n, int k, vector<int>& res){
+    void dfs(int num,int n,int k, vector<int>& res){
         if(n==0){
             res.push_back(num);
             return;
         }
         int lastdigit = num%10;
         if(lastdigit+k<=9){
-            dfs(num*10 + lastdigit+k, n-1,k,res);
+            dfs(num*10+lastdigit+k,n-1,k,res);
         }
         if(k!=0 && lastdigit-k>=0){
-            dfs(num*10 + lastdigit-k,n-1,k,res);
+            dfs(num*10+lastdigit-k,n-1,k,res);
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
