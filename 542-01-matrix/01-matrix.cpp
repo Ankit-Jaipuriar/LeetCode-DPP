cf43348ba821c9796d62e95805aaa6b761c1b035
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-09-08 13:29:16 +0530

    Time: 48 ms (81.67%) | Memory: 34.1 MB (81.89%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    bool isSafe(int i, int j, int n, int m){
+        return (i>=0 && i<n && j>=0 && j<m);
+    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
+        // source of bfs -> numbers are 0
+        int n = mat.size();
+        int m = mat[0].size();
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]!=0) mat[i][j] = n+m;
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                }else{
+                    mat[i][j]=n+m;
+                }
             }
         }
-        //left top
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]!=0){
-                if(j>0) mat[i][j] = min(mat[i][j], mat[i][j-1]+1); //left
-                if(i>0) mat[i][j] = min(mat[i][j],mat[i-1][j]+1); //top
-            }
-            }
-        }
-        // right bottom
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(mat[i][j]!=0){
-                    if(j<m-1) mat[i][j] = min(mat[i][j],mat[i][j+1]+1); //right
-                    if(i<n-1) mat[i][j] = min(mat[i][j], mat[i+1][j]+1); //bottom
+        vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty()){
+            auto curr = q.front();
+            int i = curr.first, j=curr.second;
+            q.pop();
+            for(int k=0;k<4;k++){
+                int newi = i+dir[k][0];
+                int newj = j+dir[k][1];
+                if(isSafe(newi,newj,n,m) && mat[newi][newj]>mat[i][j]+1){
+                    mat[newi][newj]=mat[i][j]+1;
+                    q.push({newi,newj});
                 }
             }
         }
