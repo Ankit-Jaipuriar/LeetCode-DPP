e8970e310da0b99f98f27d4409a30719fd321727
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-09-12 22:02:42 +0530

    Time: 2891 ms (5.09%) | Memory: 7.2 MB (97.36%) - LeetSync

--- 897-prime-palindrome/prime-palindrome.cpp
+++ 897-prime-palindrome/prime-palindrome.cpp
@@ -1,40 +1,28 @@
 class Solution {
-private:
-    bool isPrime(const int& n)
-    {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= n; i++) {
-            if (!(n % i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool isPal(int n)
-    {
-        if (n < 0) {
-            return false;
-        }
-        int original = n;
-        int reversed = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            reversed = reversed * 10 + digit;
-            n /= 10;
-        }
-        return original == reversed;
-    }
-
 public:
-    int primePalindrome(int n) 
-    {
-        while(!(isPal(n) && isPrime(n))) {
+bool isPalindrome(int n){
+    int temp = n;
+    int rem=0;
+    while(n>0){
+        int ld = n%10;
+        rem = rem*10 +ld;
+        n=n/10;
+    }
+    return temp==rem;
+}
+bool isPrime(int n){
+    if(n<=1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+    int primePalindrome(int n) {
+        while(!(isPalindrome(n) && isPrime(n))){
             n++;
         }
         return n;
     }
-
 };
