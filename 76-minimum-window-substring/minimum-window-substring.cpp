06682eed9dd052b92041a07af338cfabeb69e6c4
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-09-24 08:39:02 +0530

    Time: 21 ms (55.73%) | Memory: 11.1 MB (28.27%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mp; // Changed from int to char
+        int n = s.size();
+        int m = t.size();
+        int l = 0, r = 0;
+        int minLen = 1e9;
+        int sidx = -1;
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            mp[t[i]]++;
+        }
+        while (r < n) {
+            if (mp[s[r]] > 0) {
+                cnt++;
+            }
+            mp[s[r]]--; 
+            r++; 
+            while (cnt == m) {
+                if (r - l < minLen) {
+                    minLen = r - l;
+                    sidx = l;
+                }
+                mp[s[l]]++;
+                if (mp[s[l]] > 0) {
+                    cnt--;
+                }
+                l++; 
+            }
+        }
+
+        return sidx == -1 ? "" : s.substr(sidx, minLen);
+    }
+};
