e34a47ec204a0a5a9acff3099e920a9fa3816b85
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2024-09-27 08:26:03 +0530

    Time: 22 ms (65.72%) | Memory: 22.1 MB (76.86%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
+        int m=mat.size();
+        int n=mat[0].size();
+        int count=0,up=1;
+        int i=0,j=0;
         vector<int> ans;
-        int count=0,up=1,i=0,j=0;
         while(count<m*n){
             if(up==1){
                 while(i>=0 && j<n){
                     ans.push_back(mat[i][j]);
                     count++;
-                    if(j==n-1){
-                        i++;
-                        break;
-                    }else if(i==0){
-                        j++;
-                        break;
-                    }
-                    i--;
+                if(j==n-1){
+                    i++;
+                    break;
+                }else if(i==0){
                     j++;
+                    break;
+                }
+                i--;
+                j++;
                 }
                 up^=1;
             }else{
-                while(j>=0 && i<m){
+                 while(j>=0 && i<m){
                     ans.push_back(mat[i][j]);
                     count++;
-                    if(i==m-1){
-                        j++;
-                        break;
-                    }else if(j==0){
-                        i++;
-                        break;
-                    }
+                if(i==m-1){
+                    j++;
+                    break;
+                }else if(j==0){
                     i++;
-                    j--;
+                    break;
                 }
-                up^=1;
+                i++;
+                j--;
+                 }
+                up^=1;  
             }
         }
         return ans;
