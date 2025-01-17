8a1a72d0923ebba795ad630d491367566b891578
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-01-17 22:21:18 +0530

    Time: 0 ms (100%), Space: 18.7 MB (6.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0,-1},{0,1},{-1,0},{1,0}};
-
-    bool isSafe(int r, int c, vector<vector<int>>& image){
-        return r<image.size() && r>=0 &&  c<image[0].size() && c>=0;
-    }
-    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor, int color){
-        image[sr][sc]=color;
-        for(auto d:dir){
-            int newr = sr+d[0];
-            int newc = sc+d[1];
-
-            if(isSafe(newr, newc,image) && image[newr][newc]==oldColor){
-                dfs(image,newr,newc,oldColor,color);
-            }
-        }
+    bool isSafe(int r,int c, vector<vector<int>>& image){
+        return r>=0 && r<image.size() && c>=0 && c<image[0].size();
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if(image[sr][sc]==color) return image;
-        dfs(image,sr,sc,image[sr][sc],color);
+        queue<pair<int,int>> q;
+        int oldColor = image[sr][sc];
+        image[sr][sc]=color;
+        q.push({sr,sc});
+        vector<vector<int>> dir = {{0,-1},{0,1},{-1,0},{1,0}};
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+               q.pop();
+            for(auto d:dir){
+                int newr = r+d[0];
+                int newc = c+d[1];
+
+                if(isSafe(newr,newc,image) && image[newr][newc]==oldColor){
+                    q.push({newr,newc});
+                    image[newr][newc]=color;
+                }
+            }
+        }
         return image;
     }
 };
