c13faa77eac812fd5833f349ca6d14baffa336df
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-01-18 23:43:53 +0530

    Time: 356 ms (5.06%), Space: 63.1 MB (5.09%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{0,-1}, {0,1}, {-1,0}, {1,0}};
     bool isSafe(int i,int j,int m,int n){
         return i>=0 && i<m && j>=0 && j<n; 
     }
+
+    void dfs(vector<vector<int>>& grid, int m,int n,int i,int j){
+        grid[i][j]=0;
+
+        for(auto d:dir){
+            int newx = i+d[0];
+            int newy = j+d[1];
+
+            if(isSafe(newx,newy,m,n) && grid[newx][newy]==1){
+                dfs(grid,m,n,newx,newy);
+            }
+        }
+    }
+
     int numEnclaves(vector<vector<int>>& grid) {
         int m =  grid.size();
         int n = grid[0].size();
-
-        queue<pair<int,int>> q;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 || i==m-1 || j==0 || j==n-1){
                     if(grid[i][j]==1){
-                        q.push({i,j});
-                        grid[i][j]=0;
+                       dfs(grid,m,n,i,j);
                     }
                 }
             }
         }
 
-        vector<vector<int>> dir = {{0,-1}, {0,1}, {-1,0}, {1,0}};
 
-        while(!q.empty()){
-            int sz = q.size();
-            for(int i=0;i<sz;i++){
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-
-                for(auto d:dir){
-                    int newx = x+d[0];
-                    int newy = y+d[1];
-
-                    if(isSafe(newx,newy,m,n) && grid[newx][newy]==1){
-                        grid[newx][newy]=0;
-                        q.push({newx,newy});
-                    }
-                }
-            }
-        }
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
