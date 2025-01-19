ccf3ae30da44c8d88c01876eb999ab1039da5cd3
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-01-19 22:53:39 +0530

    Time: 206 ms (53.85%), Space: 257.6 MB (53.85%) - LeetHub

--- 3737-paint-house-iv/3737-paint-house-iv.cpp
+++ 3737-paint-house-iv/3737-paint-house-iv.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
     long long minCost(int n, vector<vector<int>>& cost) {
-        vector<vector<vector<long long>>> dp(n / 2, vector<vector<long long>>(3, vector<long long>(3, LLONG_MAX)));
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i != j) {
-                    dp[0][i][j] = cost[0][i] + cost[n-1][j];
-                }
+       vector<vector<vector<long long>>> dp(n/2, vector<vector<long long>>(3, vector<long long>(3, LLONG_MAX)));
+
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i!=j){
+                dp[0][i][j]=cost[0][i] + cost[n-1][j];
             }
         }
+       }
 
-        for (int row = 1; row < n / 2; row++) {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (i == j) {
-                        continue;
-                    } 
-                    
+        for(int row=1;row<n/2;row++){
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(i==j) continue;
+
                     long long minPrev = LLONG_MAX;
-                    
-                    for (int i1 = 0; i1 < 3; i1++) {
-                        for (int j1 = 0; j1 < 3; j1++) {
-                            if (i1 != i && j1 != j && i1 != j1) {
-                                minPrev = min(minPrev, dp[row-1][i1][j1]);
+
+                    for(int i1=0;i1<3;i1++){
+                        for(int j1=0;j1<3;j1++){
+                            if(i1!=i && j1!=j){
+                            minPrev = min(minPrev, dp[row-1][i1][j1]);
                             }
                         }
                     }
-                    
+
                     dp[row][i][j] = minPrev + cost[row][i] + cost[n-row-1][j];
                 }
             }
         }
 
-        long long result = LLONG_MAX;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i != j) {
-                    result = min(result, dp[n/2-1][i][j]);
+        long long res = LLONG_MAX;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(i!=j){
+                    res = min(res,dp[n/2-1][i][j]);
                 }
             }
         }
+        return res;
 
-        return result;
     }
 };
