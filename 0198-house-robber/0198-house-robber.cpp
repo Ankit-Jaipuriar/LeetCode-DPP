c5a69e7609e01ec44144af1a37eb01040026f5e7
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-02-09 00:00:26 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.43%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        nums.push_back(0);
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;  // base case
+        dp[1] = nums[0];  
+        dp[2] = max(nums[1],nums[0]);
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = max(dp[i - 1] , dp[i - 2] + nums[i - 1]);
+        }
+        
+        return dp[n]; 
+    }
+};
