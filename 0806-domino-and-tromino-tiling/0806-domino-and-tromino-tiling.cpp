ef5cc2059022be9536734d9eaace1129aadb2629
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-02-21 23:23:18 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.47%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,24 +1,28 @@
 long long int dp[1000+5][5];
-int u = 1e9+7 ; 
+int MOD=1e9+7;
 class Solution {
 public:
     int numTilings(int n) {
-        dp[1][1] = 1 ; 
-        dp[2][1] = 2 ; 
-        
-        dp[2][0] = 1 ; 
-        dp[2][2] = 1 ; 
+    // base case for domino
+     dp[1][1]=1;
+     dp[2][1]=2;
 
-        int i = 3 ;
-        while(i<=n){
+    // base case for upper tromino
+    dp[2][0]=1;
+    // base case for down tromino
+    dp[2][2]=1; 
+    int i=3;
 
-            dp[i][1] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][0] + dp[i-2][1])%u ; 
-            dp[i][0] = (dp[i-2][1] + dp[i-1][2])%u ; 
+    while(i<=n){
+        dp[i][1]=(dp[i-1][1]+dp[i-2][1]+dp[i-1][2]+dp[i-1][0])%MOD; //when we put domino at ith pos
 
-            dp[i][2] = (dp[i-2][1] + dp[i-1][0])%u ; 
-            i++;
-        }
-        return dp[n][1]%u ;
-        //RMRM..... 
+        // upper tromino at i
+        dp[i][0]=(dp[i-2][1]+dp[i-1][2])%MOD;
+
+        // down tromino at i
+        dp[i][2]=(dp[i-2][1]+dp[i-1][0])%MOD;
+    i++;
+    }
+    return dp[n][1]%MOD;
     }
 };
