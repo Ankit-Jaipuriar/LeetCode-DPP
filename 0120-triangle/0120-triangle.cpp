832a28fda60951aa32ba8844dcec02838cc7f385
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-03-02 18:20:02 +0530

    Time: 0 ms (100%), Space: 12.6 MB (73.76%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = triangle[0][0]; // Initialize with the top element of the triangle
+        int n=triangle.size();
+        vector<int> dp(n+1,INT_MAX);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j >= 0; j--) { // Traverse from right to left to avoid overwriting
-                if (j == 0) {
-                    dp[j] = dp[j] + triangle[i][j];
-                } else if (j == i) {
-                    dp[j] = dp[j - 1] + triangle[i][j];
-                } else {
-                    dp[j] = min(dp[j], dp[j - 1]) + triangle[i][j];
+        dp[0]=triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=i;j>=0;j--){
+                // j==0, means first element in current roww
+                if(j==0){
+                    dp[j]=dp[j]+triangle[i][j];
+                }else if(j==i){ //j==i means last element in current row
+                    dp[j]=dp[j-1]+triangle[i][j];
+                }else{ //mid element can come from left or right diagonal
+                    dp[j]=min(dp[j],dp[j-1]) + triangle[i][j];
                 }
             }
         }
-        return *min_element(dp.begin(), dp.begin() + n);
+        // dp[i] --> indicated min path to reach the current index in current row
+        // there are different path to reach the last row, hence we take min of all of them
+        return *min_element(dp.begin(),dp.end());
     }
 };
