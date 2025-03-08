89e17050ddd57fe93ef88d4bff170ebca51a641a
Ankit Kumar Jaipuriar <ankitjaipuriar003@gmail.com>
2025-03-08 11:51:55 +0530

    Time: 0 ms (100%), Space: 18.1 MB (52.64%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,63 +1,68 @@
 class Solution {
 public:
-
-    struct Node {
+    struct node{
         int distance;
         int vertex;
         int stops;
     };
 
-    struct cmp {
-        bool operator()(Node a, Node b) {
-            if (a.distance == b.distance) {
-                return a.vertex > b.vertex; 
+    struct cmp{
+        bool operator()(node a, node b){
+            if(a.distance==b.distance){
+                return a.vertex>b.vertex;
             }
-            return a.distance > b.distance; 
+            return a.distance>b.distance;
         }
     };
 
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-       vector<vector<pair<int, int>>> graph(n);
-        for (auto i : flights) {
-            graph[i[0]].push_back({i[1], i[2]});
+
+        vector<vector<pair<int,int>>> graph(n);
+        for(int i=0;i<flights.size();i++){
+            int x,y,z;
+            x=flights[i][0];
+            y=flights[i][1];
+            z=flights[i][2];
+            graph[x].push_back({y,z});
         }
 
-        // Use 2D vector to track the minimum distance to a node with a given number of stops
-        vector<vector<int>> distance(n, vector<int>(k + 2, INT_MAX));
-        distance[src][0] = 0;
+        priority_queue<node, vector<node>, cmp> pq;
+        pq.push({0,src,0});
 
-        priority_queue<Node, vector<Node>, cmp> pq;
-        pq.push({0, src, 0});
+        vector<vector<int>> distance(n, vector<int>(k+2,INT_MAX));
+        distance[src][0]=0;
 
-        while (!pq.empty()) {
-            int currDistance = pq.top().distance;
-            int currVertex = pq.top().vertex;
-            int currStops = pq.top().stops;
+        while(!pq.empty()){
+            int currdist=pq.top().distance;
+            int currvert=pq.top().vertex;
+            int currstops=pq.top().stops;
             pq.pop();
 
-            if (currVertex == dst) {
-                return currDistance;
+            if(currvert==dst){
+                return currdist;
             }
 
-            if (currStops > k) {
+            if(currstops>k){
                 continue;
             }
 
-            for (auto u : graph[currVertex]) {
-                int neighbor = u.first;
-                int weight = u.second;
-                if (currDistance + weight < distance[neighbor][currStops + 1]) {
-                    distance[neighbor][currStops + 1] = currDistance + weight;
-                    pq.push({distance[neighbor][currStops + 1], neighbor, currStops + 1});
+            for(auto nb:graph[currvert]){
+                int nbr=nb.first;
+                int weight=nb.second;
+
+                if(currdist+weight<distance[nbr][currstops+1]){
+                    distance[nbr][currstops+1]=currdist+weight;
+                    pq.push({distance[nbr][currstops+1], nbr, currstops+1});
                 }
             }
         }
 
-        int minDistance = INT_MAX;
-        for (int i = 0; i <= k + 1; i++) {
-            minDistance = min(minDistance, distance[dst][i]);
+        int mini=INT_MAX;
+        for(int i=0;i<=k+1;i++){
+            mini=min(mini, distance[dst][i]);
         }
 
-        return minDistance == INT_MAX ? -1 : minDistance;
+        return mini==INT_MAX ? -1:mini;
+
     }
 };
